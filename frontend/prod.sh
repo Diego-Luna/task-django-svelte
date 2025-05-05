f7b342177f92fd73975b5c0e960f7a28d8f18564
Diego-Luna <diegofcolunalopez@gmail.com>
2025-05-05 19:14:28 +0200

    clean the code

--- frontend/prod.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Asegúrate de tener una clave secreta segura para producción
-if [ -z "$DJANGO_SECRET_KEY" ]; then
-  echo "DJANGO_SECRET_KEY no está definida. Generando una clave temporal..."
-  export DJANGO_SECRET_KEY=$(openssl rand -base64 32)
-fi
-
-docker-compose -f docker-compose.yml -f docker-compose.prod.yml up -d "$@"
